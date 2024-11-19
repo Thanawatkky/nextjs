5ae26d29c4de2cda1a8d1a428ffcb0508c350d14
Thanawatkky <wiko848287@gmail.com>
2024-11-19 16:04:52 +0700

    [UPDATE] Product page

--- src/app/(main-website)/product/page.tsx
+++ src/app/(main-website)/product/page.tsx
@@ -1,63 +1,43 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Container } from '@mui/material';
+import Link from 'next/link'
+import { Container, Divider, Grid, Paper, Typography } from '@mui/material'
+import Image from 'next/image'
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
+async function  getData() {
+  const response = await fetch("https://dummyjson.com/products",{cache: "no-store"} )
+  if(!response.ok) {
+    throw new Error("ไม่สามารถ fetch Data ได้")
+  }
+  return response.json()
 }
+export default async function BasicTable() {
+  const response = await getData()
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
   return (
     <Container>
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </Container>
+    <h1>Product</h1>
+    {
+      response.products && (
+        <>
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+              {
+                response.products.map((item: any) => (
+                  <Grid key={item.id} lg={3} xs={6} sx={{ textAlign: "center"}}>
+                      <Paper sx={{ margin: "1rem", ":hover": {padding: "1.5rem"}, transition: ".4s"}}>
+                        <Image src={item.thumbnail} alt={item.detail} width={120} height={120} />
+                        <Divider></Divider>
+                        <Typography sx={{ padding: "1rem"}}>{item.title}</Typography>
+                      </Paper>
+                  </Grid>
+                ))
+              }
+          </Grid>
+        </>
+      )
+    }      
+    <br />
+    <Link style={{margin: "0 2rem 0 0 "}} href={"/about"}>About Page </Link>
+    <Link href={"/content/resume"}>Resume Page </Link>
+  </Container>
   );
 }
